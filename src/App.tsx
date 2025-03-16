3e29133e374d4b19fcfb42158165fca6d26a8be4
madza <mr.ghasemi.1375@gmail.com>
2025-03-16 14:01:05 +0330

    adding add timer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import Button from "./components/button"
-import Form from "./components/Form"
-import Input from "./components/Input"
+import AddTimer from "./components/AddTimer"
 
 
 function App(){
-
-  function handleOnSave(data:unknown){
-    const extractedData = data as {title:string , price:string}
-
-    console.log(extractedData)
-  }
   return(
-      <div>
-        <Form onSave={handleOnSave}>
-          <Input id="title" type="text" placeholder="Title" />
-          <Input id="price" type="text" placeholder="Price" />
-          <Button>Add Product</Button>
-        </Form>
-      </div>
+    <div>
+      <AddTimer/>
+    </div>
   )
+  
 }
 
 export default App
---
+++ src/components/AddTimer.tsx
@@ -0,0 +1,23 @@
+import Input from "./Input"
+import Form from "./Form"
+import Button from "./button"
+
+function AddTimer(){
+    function handleOnSave(data:unknown){
+        const extractedData = data as {name:string , duration:string}
+    
+        console.log(extractedData)
+      }
+      return(
+          <div>
+            <Form onSave={handleOnSave} className="bg-slate-800 flex flex-col p-8 max-w-xl rounded-md mx-auto gap-5">
+              <Input id="name" type="text" placeholder="Name" className="p-2 bg-slate-600 w-full outline-none rounded-md"/>
+              <Input id="duration" type="text" placeholder="Duration" className="p-2 bg-slate-600 w-full outline-none rounded-md" />
+              <Button className="p-2 bg-slate-900 rounded-md text-slate-200">Add Timer</Button>
+            </Form>
+          </div>
+      )
+}
+
+
+export default AddTimer
