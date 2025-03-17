79b2ace084c34c0243ced30c991d0cb4bdf905ce
madza <mr.ghasemi.1375@gmail.com>
2025-03-17 18:15:31 +0330

    reducer incomplete

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,46 +1,71 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useReducer } from "react";
 
-type Timer ={
-    name: string
-    duration: number
+type Timer = {
+  name: string;
+  duration: number;
+};
+
+type TimersState = {
+  isRunning: boolean;
+  timers: Timer[];
+};
+
+const initialState:TimersState ={
+    isRunning: true,
+    timers: []
 }
 
-type TimersState ={
-    isRunning: boolean
-    timers:Timer[]
+type TimersContextValue = TimersState & {
+  addTimers: (timerData: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
+};
+
+const TimersContext = createContext<TimersContextValue | null>(null);
+
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+
+  if (timersCtx === null) {
+    throw new Error("TimersContext is null");
+  }
+  return timersCtx;
 }
 
-type TimersContextValue = TimersState &{
-    addTimers: (timerData:Timer)=> void
-    startTimers: ()=> void
-    stopTimers: ()=> void
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+
+type Action ={
+    type: 'ADD-TIMER' | 'START-TIMERS' | 'STOP-TIMERS'
 }
 
-const TimersContext = createContext<TimersContextValue | null>(null)
+function timersReducer (state: TimersState, action:Action): TimersState{
 
-export function useTimersContext(){
-    const timersCtx = useContext(TimersContext)
-
-    if(timersCtx === null){
-        throw new Error('TimersContext is null')
-    }
-    return timersCtx
 }
 
-type TimersContextProviderProps={
-    children: ReactNode
-}
+export default function TimersContextProvider({
+  children,
+}: TimersContextProviderProps) {
+    const [timersState, dispatch] = useReducer(timersReducer, initialState)
 
-export default function TimersContextProvider({children}:TimersContextProviderProps){
-    const ContextValue:TimersContextValue ={
-        isRunning:true,
-        timers:[],
-        addTimers (timerData){},
-        startTimers() {},
-        stopTimers(){}
-    }
+  const ContextValue: TimersContextValue = {
+    isRunning: true,
+    timers: [],
+    addTimers(timerData) {
+        dispatch({type: 'ADD-TIMER'})
+    },
+    startTimers() {
+        dispatch({type:'START-TIMERS'})
+    },
+    stopTimers() {
+        dispatch({type: 'STOP-TIMERS'})
+    },
+  };
 
-    return(
-        <TimersContext.Provider value={ContextValue}>{children}</TimersContext.Provider>
-    )
+  return (
+    <TimersContext.Provider value={ContextValue}>
+      {children}
+    </TimersContext.Provider>
+  );
 }
