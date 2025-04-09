f722fd3c75b6f222ae4fcee4a6b1ace4141e84eb
madza-dell <mr.ghasemi.1375@gmail.com>
2025-04-10 00:27:18 +0330

    get posts from api and show it on dom using postlist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
-import AddTimer from "./components/AddTimer"
-import TimersControl from "./components/TimersControl"
-import TimersContextProvider from "./store/timers-context"
-import Timers from "./components/Timers"
+import { useState , useEffect, type ReactNode } from "react"
+import { get } from "./util/http"
+import PostList from "./components/PostList"
 
+type Post ={
+  id: number
+  title: string
+  text: string
+}
+
+type Data ={
+  userId: number
+  id: number
+  title: string
+  body: string
+}
 
 function App(){
+  const[fetchedPosts,setFetchedPosts] = useState<Post[]>()
+
+  useEffect(()=>{
+    async function fetchData(){
+      const data = await get('https://jsonplaceholder.typicode.com/posts') as Data[]
+
+      const blogPosts: Post[] = data.map((item)=>{
+        return{
+          id: item.id,
+          title: item.title,
+          text: item.body
+        }
+      })
+      setFetchedPosts(blogPosts)
+    }
+    fetchData()
+  },[])
+
+  let content: ReactNode
+
+  if(fetchedPosts){
+    content = <PostList posts={fetchedPosts}/>
+  }
+  
   return(
-    <TimersContextProvider>
-      <div>
-        <TimersControl></TimersControl>
-        <AddTimer/>
-        <Timers/>
-      </div>
-    </TimersContextProvider>
-  )
+
+      <div>{content}</div>
+ )
   
 }
 
